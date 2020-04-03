505fd5217044d061b994aede0844c6f476489009
Samuel Pampolini <samuelpampolini@gmail.com>
2020-04-03 15:33:20 +0100

    League button toggle and past the extra to skill

--- app/src/main/java/com/samuelpampolini/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/samuelpampolini/swoosh/LeagueActivity.kt
@@ -3,8 +3,12 @@ package com.samuelpampolini.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,7 +16,34 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun leagueNextClicked(view: View) {
+        if(selectedLeague == "") {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         var skillActivity = Intent(this, SkillActivity::class.java)
+        skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
         startActivity(skillActivity)
+    }
+
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coEdLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coEdLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoEdClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "co-ed"
     }
 }
