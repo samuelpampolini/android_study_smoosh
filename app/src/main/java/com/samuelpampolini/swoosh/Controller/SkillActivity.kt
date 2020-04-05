65d29fe9c2db59d1136ff574947e9eb631f558e6
Samuel Pampolini <samuelpampolini@gmail.com>
2020-04-05 12:34:49 +0100

    Create icone and save state on the activity to save state

--- app/src/main/java/com/samuelpampolini/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/samuelpampolini/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.samuelpampolini.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.samuelpampolini.swoosh.Model.Player
@@ -11,6 +12,18 @@ import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
     private lateinit var player: Player
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
